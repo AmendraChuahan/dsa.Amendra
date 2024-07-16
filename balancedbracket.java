1abca3aec26c8836242c928c0f6fc73fa3a7d822
Amendra singh <167674924+Amendrar@users.noreply.github.com>
2024-07-17 00:32:04 +0530

    Add files via upload

---
+++ balancedbracket.java
@@ -0,0 +1,30 @@
+package stacks;
+import java.util.*;
+public class balancedBracket {
+    public static boolean isbalanced(String str) {
+        Stack<Character> st = new Stack<>();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);// character ki value ko fetch karega
+            if (ch =='(') {
+                st.push(ch);
+            }
+
+                else{
+                   if (st.size()==0) return false;
+                if(st.peek()=='(') st.pop();
+            }
+        }
+        if(st.size()>0) return  false;//
+else return true;
+    }
+
+        public static void main (String[]args){
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            System.out.println(isbalanced(str));
+
+        }
+    }
+
+
